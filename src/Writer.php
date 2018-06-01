e723450383410ebe5f8e7ede6a3df93f76f22616
hacpaka <hacpaka@eggbe.com>
2018-06-01 04:19:01 +0300

    writing modes added

--- src/Writer.php
+++ src/Writer.php
@@ -25,17 +25,42 @@ class Writer implements IWriter {
 	}
 
 	/**
+	 * @const int
+	 */
+	public const WM_SKIP_EMPTY = 0b0001;
+
+	/**
+	 * @const int
+	 */
+	public const WM_SKIP_INDENT = 0b0010;
+
+	/**
 	 * @param Generator $Input
+	 * @param int $mode
 	 * @throws \Exception
 	 */
-	public final function write(\Generator $Input): void {
+	public final function write(\Generator $Input, int $mode = 0): void {
 		if (!is_resource($handler = fopen($this->File->toString(), 'a'))){
 			throw new \Exception('Invalid source!');
 		}
 
 		try{
 			foreach ($Input as $line) {
-				fputs($handler, rtrim($line) . "\n");
+				/**
+				 * If the WP_SKIP_INDENT flag is set any leading
+				 * or ending whitespace characters will be removed.
+				 */
+				if ($mode & self::WM_SKIP_INDENT){
+					$line = trim($line);
+				}
+
+				/**
+				 * if the WP_SKIP_EMPTY flag is set, any empty strings
+				 * will be ignored.
+				 */
+				if (!empty(trim($line)) || !($mode & self::WM_SKIP_EMPTY)) {
+					fputs($handler, rtrim($line) . "\n");
+				}
 			}
 		}finally{
 			fclose($handler);
