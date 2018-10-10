806bee3d9189d51e8afb7c8320ba96c0f00477c6
hacpaka <hacpaka@eggbe.com>
2018-10-10 10:18:31 +0300

    refactoring

--- src/ReadingBuffer.php
+++ src/ReadingBuffer.php
@@ -48,7 +48,7 @@ class ReadingBuffer extends ABuffer
 	 * @throws \Exception
 	 */
 	public final function read(): \Generator {
-		return (new Regexp('/\r?\n/'))->split($this->Buffer);
+		yield from Regexp::create('/(?:\r\n|\n|\r)/')->split($this->Buffer);
 	}
 
 	/**
