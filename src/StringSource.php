3daff24f25aa5a440bff8f66bc602a9f10217b7c
hacpaka <hacpaka@eggbe.com>
2019-02-08 15:03:41 +0300

    +

--- src/StringSource.php
+++ src/StringSource.php
@@ -2,6 +2,7 @@
 namespace Able\IO;
 
 use \Able\IO\Abstractions\ISource;
+use \Able\IO\ReadingBuffer;
 
 class StringSource
 	implements ISource {
@@ -23,5 +24,12 @@ class StringSource
 	 */
 	public final function getContent(): string {
 		return $this->content;
+	}
+
+	/**
+	 * @return ReadingBuffer
+	 */
+	public final function toReadingBuffer(): ReadingBuffer {
+		return new ReadingBuffer($this);
 	}
 }
